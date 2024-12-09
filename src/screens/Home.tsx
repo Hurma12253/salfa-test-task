be7bed8a116240328a6f73f7af08c318d0951008
Hurma12253 <slava1511200@gmail.com>
2024-12-09 11:48:56 +0500

    feat: add home page

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,8 +1,13 @@
+import { Button, Container } from '@mui/material'
 import React from 'react'
+import { Link } from 'react-router'
+import { Paths } from '../router/constants'
 
 const Home: React.FC = () => {
     return (
-        <div>Home</div>
+        <Container>
+            <Button component={Link} to={Paths.products}>Go to products</Button>
+        </Container>
     )
 }
 
