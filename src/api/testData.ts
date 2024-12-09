47f465e989ea4fd9d11c294e8ab36a5ddd33a8e1
Hurma12253 <slava1511200@gmail.com>
2024-12-09 13:11:00 +0500

    feat: add mock data

---
+++ src/api/testData.ts
@@ -0,0 +1,69 @@
+export interface ITestData {
+    image: string
+    title: string
+    description: string
+    liked: boolean
+}
+
+export const testData: ITestData[] = [
+    {
+        "image": "https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0",
+        "title": "Солнечный закат",
+        "description": "Красивый закат над океаном.",
+        "liked": true
+    },
+    {
+        "image": "https://images.unsplash.com/photo-1506784983877-45594efa4cbe",
+        "title": "Городской пейзаж dqwdd wqqwddq wdqwd qd qqwdqdq wqd",
+        "description": "Высотные здания мегаполиса в ночное время с яркими огнями, отражающимися в реке.wqwdqwddddddddddddddddddddddddddddddddddddddddddasasdasdadasdadasdaddadasddada",
+        "liked": false
+    },
+    {
+        "image": "https://images.unsplash.com/photo-1529917596152-5d2d486d6f5d",
+        "title": "Лесная тропинка",
+        "description": "Тихая тропинка в лесу, окруженная деревьями и цветами, подходит для прогулок на свежем воздухе.",
+        "liked": true
+    },
+    {
+        "image": "https://images.unsplash.com/photo-1501785888041-af3ef285b470",
+        "title": "Горный пик",
+        "description": "Высокий заснеженный пик на фоне ярко-голубого неба.",
+        "liked": false
+    },
+    {
+        "image": "https://images.unsplash.com/photo-1512920161249-e5c6f40cbf84",
+        "title": "Водопад в тропиках",
+        "description": "Мощный поток воды спадает с высокой скалы, окруженной тропической зеленью.",
+        "liked": true
+    },
+    {
+        "image": "https://images.unsplash.com/photo-1519181245277-cffeb31da2a5",
+        "title": "Корабль на горизонте",
+        "description": "Маленький корабль дрейфует по бескрайнему морю. Густые облака наполняют небо, создавая драматичный фон.",
+        "liked": false
+    },
+    {
+        "image": "https://images.unsplash.com/photo-1506224774997-84be6a5e6e04",
+        "title": "Поляна цветов",
+        "description": "Живописная поляна, покрытая множеством разноцветных цветов, окруженная холмами и лесом. Это место идеально подходит для отдыха на природе.",
+        "liked": true
+    },
+    {
+        "image": "https://images.unsplash.com/photo-1517817748491-74e9ae20bc46",
+        "title": "Звездное небо",
+        "description": "Темное ночное небо, усыпанное миллиардами звезд, и туманность, мерцающая вдалеке.",
+        "liked": false
+    },
+    {
+        "image": "https://images.unsplash.com/photo-1493558103817-58b2924bce98",
+        "title": "Пляж с белым песком",
+        "description": "Безмятежный пляж с белоснежным песком, бирюзовой водой и покачивающимися пальмами на берегу.",
+        "liked": true
+    },
+    {
+        "image": "https://images.unsplash.com/photo-1472214103451-9374bd1c798e",
+        "title": "Осенний лес",
+        "description": "Лесная тропа, усеянная листьями, окрашенными в яркие осенние цвета. Деревья окружены мягким золотым светом заката.",
+        "liked": false
+    }
+]
